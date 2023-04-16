f998d6bef5dd3f808c1f8a81f352edfc343d4ef3
Son Dang <minhson0506@gmail.com>
2023-04-16 16:46:12 +0300

    change listen port to 1337

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import mongoConnect from './utils/db';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1337;
 (async () => {
   try {
     await mongoConnect();
